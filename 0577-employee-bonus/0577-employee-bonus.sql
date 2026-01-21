16a40b947c464b805d2912b8690053d5db212019
Devang Dhaka <devangdhaka.ai@gmail.com>
2026-01-21 10:14:15 +0530

    Time: 360 ms (16.82%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,12 @@
 -- Write your PostgreSQL query statement below
-SELECT e.name, b.bonus 
+-- SELECT e.name, b.bonus 
+-- FROM Employee e
+-- LEFT JOIN Bonus b 
+-- ON e.empId = b.empId
+-- WHERE b.Bonus < 1000 OR b.Bonus IS NULL;
+
+SELECT e.name,
+       COALESCE(b.bonus, NULL) AS bonus
 FROM Employee e
-LEFT JOIN Bonus b 
-ON e.empId = b.empId
-WHERE b.Bonus < 1000 OR b.Bonus IS NULL;
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
